a78cb4eb66cb04c79ceb4964c44d265a8d797db9
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2022-08-31 15:11:23 +0200

    feat: state and header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,13 +1,69 @@
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
 // == Import
-import './styles.css';
+import Header from 'src/components/Header';
+import Toggler from 'src/components/Toggler';
+import Currencies from 'src/components/Currencies';
+import Result from 'src/components/Result';
 
-// == Composant
-function App() {
-  return (
-    <div className="app">
-      <h1>Composant : App</h1>
-    </div>
-  );
+// import du fichier scss
+import './app.scss';
+
+// import de mes données statiques
+import currencyData from 'src/data/currencies';
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    // je déclare l'état
+    this.state = {
+      // le montant a convertir
+      baseAmount: 1,
+      // la devise cible selectionnée
+      selectedCurrency: 'Swiss Franc',
+      // le champ de recherche de devise
+      inputSearch: '',
+    };
+
+    // j'utilise la méthode bind pour ne pas perdre la valeur de this
+    this.handleBaseAmountChange = this.handleBaseAmountChange.bind(this);
+  }
+
+    // une fonction appellée lors du clic sur une devise
+  // la fonction recoit en parametre la nouvelle devise selectionnée
+  handleCurrencyClick(newCurrency) {
+    this.setState({
+      selectedCurrency: newCurrency,
+    });
+  }
+
+  handleBaseAmountChange(event) {
+    this.setState({
+      baseAmount: event.target.valueAsNumber,
+    });
+  }
+
+  handleSearchInputChange(event) {
+    this.setState({
+      inputSearch: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Header
+          baseAmount={this.state.baseAmount}
+          onBaseAmountChange={this.handleBaseAmountChange}
+        />
+        {/* un composant pour activer/désactiver la liste */}
+
+      </div>
+    );
+  }
 }
 
 // == Export
---
+++ src/components/Header/index.js
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+
+// import du fichier scss
+import './header.scss';
+
+function Header({ baseAmount, onBaseAmountChange }) {
+  return (
+    <header className="header">
+      <h1 className="header__title">€ Converter</h1>
+      <input
+        className="header__input"
+        type="number"
+        min="0"
+        max="10000"
+        placeholder="Montant a convertir en euros"
+        value={baseAmount}
+        onChange={onBaseAmountChange}
+      />
+    </header>
+  );
+}
+
+Header.propTypes = {
+  baseAmount: PropTypes.number.isRequired,
+  onBaseAmountChange: PropTypes.func.isRequired,
+};
+
+export default Header;
