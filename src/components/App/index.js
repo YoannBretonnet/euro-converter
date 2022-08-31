33058e00f29c4c1de5807b1eb6520ae3f6cf5d0d
YoannBretonnet <88546350+YoannBretonnet@users.noreply.github.com>
2022-08-31 13:18:10 +0200

    project preparation

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,12 +1,10 @@
 // == Import
-import reactLogo from './react-logo.svg';
 import './styles.css';
 
 // == Composant
 function App() {
   return (
     <div className="app">
-      <img src={reactLogo} alt="react logo" />
       <h1>Composant : App</h1>
     </div>
   );
